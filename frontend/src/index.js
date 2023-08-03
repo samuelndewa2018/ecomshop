c1f79c04cb048c09ef3078adea658b3183f01431
Bramuels <samuelndewa2018@gmail.com>
2023-08-03 13:04:29 -0700

    cookies

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import Store from "./redux/store";
+import Cookies from "./components/Cookies";
 
 ReactDOM.render(
   <Provider store={Store}>
+    <Cookies />
     <App />
   </Provider>,
   document.getElementById("root")
