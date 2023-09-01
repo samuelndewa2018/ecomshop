75ed8d2db18eec66c3739092595f91609ba8be50
Bramuels <samuelndewa2018@gmail.com>
2023-09-01 08:07:50 -0700

    balancing card

--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -400,7 +400,7 @@ const Hero = () => {
               </div>
 
               <div className="max-h-[120px]">
-                <div class="w-full p-2 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div class="w-full p-2 my-4 lg:my-0 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                   <ul class="space-y-1 space-x-1 grid grid-cols-2">
                     <li>
                       <Link
