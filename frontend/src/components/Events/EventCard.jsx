05ac33799ebb29accbf0e7ec439501dbda134268
Bramuels <samuelndewa2018@gmail.com>
2023-09-21 03:55:55 -0700

    epage event

--- frontend/src/components/Events/EventCard.jsx
+++ frontend/src/components/Events/EventCard.jsx
@@ -27,12 +27,7 @@ const EventCard = ({ active, data }) => {
   };
   return (
     <>
-      {/* <div
-        className={`w-full block bg-white rounded-lg ${
-          active ? "unset" : "mb-12"
-        } lg:flex p-2`}
-      > */}
-      <div class="container my-24 mx-auto md:px-6 xl:px-32">
+      <div class="container my-24">
         <section class="mb-32">
           <div class="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
             <div class="flex flex-wrap items-center">
@@ -123,7 +118,6 @@ const EventCard = ({ active, data }) => {
           </div>
         </section>
       </div>
-      {/* </div> */}
     </>
   );
 };
